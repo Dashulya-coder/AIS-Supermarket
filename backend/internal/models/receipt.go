d9e2f4ecc45cd9f4904c16133800e0e72aca2c7a
Dashulya-coder <dasha.ukshe@gmail.com>
2026-04-18 17:18:53 +0300

    feat: add receipt models and create receipt request structures

--- backend/internal/models/receipt.go
+++ backend/internal/models/receipt.go
@@ -1,1 +1,20 @@
 package models
+
+type Receipt struct {
+	ReceiptNumber string  `json:"receipt_number" db:"receipt_number"`
+	CashierID     string  `json:"cashier_id" db:"cashier_id"`
+	CardNumber    *string `json:"card_number" db:"card_number"`
+	PrintDate     string  `json:"print_date" db:"print_date"`
+	SumTotal      float64 `json:"sum_total" db:"sum_total"`
+	Vat           float64 `json:"vat" db:"vat"`
+}
+
+type CreateReceiptItemInput struct {
+	UPC           string `json:"upc"`
+	ProductNumber int    `json:"product_number"`
+}
+
+type CreateReceiptRequest struct {
+	CardNumber *string                  `json:"card_number"`
+	Items      []CreateReceiptItemInput `json:"items"`
+}
