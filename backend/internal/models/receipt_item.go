31fb0710974ca5faa3300cb9684738df57a67668
boss <a@no.pics>
2026-04-18 17:18:53 +0300

    feat: add receipt models and create receipt request structures

--- backend/internal/models/receipt_item.go
+++ backend/internal/models/receipt_item.go
@@ -1,1 +1,9 @@
 package models
+
+type ReceiptItem struct {
+	ID            int     `json:"id" db:"id"`
+	ReceiptNumber string  `json:"receipt_number" db:"receipt_number"`
+	UPC           string  `json:"upc" db:"upc"`
+	ProductNumber int     `json:"product_number" db:"product_number"`
+	SellingPrice  float64 `json:"selling_price" db:"selling_price"`
+}
